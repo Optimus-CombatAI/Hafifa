9a09db90c0a98862228f76b78983716189a193ce
Tony <ynot654@gmail.com>
2024-12-12 13:34:19 +0200

    feat(logging.py): setup logger for the api

---
+++ rolling_exercise/utils/logging.py
@@ -0,0 +1,21 @@
+import logging
+from ..config import settings
+
+
+def setup_logger():
+    logger = logging.getLogger("air_quality_api_logger")
+    log_level = getattr(logging, settings.LOG_LEVEL.upper(), logging.INFO)
+    logger.setLevel(log_level)
+
+    file_handler = logging.FileHandler(settings.LOG_FILE)
+    file_handler.setLevel(log_level)
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+air_quality_api_logger = setup_logger()
