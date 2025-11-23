f24a2d2c3153c690ef1b1dc7037d2c049ab8d6a4
Maxim <wainermaxim@gmail.com>
2025-11-23 20:43:24 +0200

    add changes

--- clean_code/player.py
+++ clean_code/player.py
@@ -6,45 +6,67 @@ from tkinter import *
 
 import random
 from dataclasses import dataclass
-from typing import Dict
+from pathlib import Path
+
+import logging
+
 
 pygame.init()
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 BUTTON_BG = 'AntiqueWhite1'
 BUTTON_WIDTH = 40
 
 
 def get_random_artist() -> str:
-    artist = ["Eminem", "JayZ", "2Pac", "Bob Dylan", "Madonna", "Rihanna", "Cher"]
-    rand_artist = artist[random.randint(0, len(artist) - 1)]
+    artists = ["Eminem", "JayZ", "2Pac", "Bob Dylan", "Madonna", "Rihanna", "Cher"]
+    rand_artist = artists[random.randint(0, len(artists) - 1)]
+
     return rand_artist
 
 
-def get_song_name(file_path) -> str:
-    len_file_type = len(".mp3")
-    return file_path.split("/")[-1][:-len_file_type]
+def get_song_name(song_file_path) -> str:
+
+    return Path(song_file_path).stem
+
+
+def play_music(directory, song_end):
+    pygame.mixer.music.load(directory)
+    pygame.mixer.music.play(1, 0.0)
+    pygame.mixer.music.set_endevent(song_end)
 
 
 @dataclass
 class Song:
-    song_path: str
-    song_metadata: Dict[str, str]
+    path: str
+    title: str
+    artist: str
 
 
 class FrameApp(Frame):
     def __init__(self, master):
         super(FrameApp, self).__init__(master)
         self.grid()
-        play_btn = Button(self, text="PLAY SONG", command=play_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
-        play_btn.grid(row=2, column=0)
-        prev_btn = Button(self, text="PREVIOUS SONG", command=play_prev_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
-        prev_btn.grid(row=4, column=0)
-        pause_btn = Button(self, text="PAUSE/UNPAUSE", command=pause_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
-        pause_btn.grid(row=3, column=0)
-        next_btn = Button(self, text="NEXT SONG", command=play_next_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
-        next_btn.grid(row=5, column=0)
-        add_btn = Button(self, text="ADD TO LIST", command=add_songs_to_list, bg=BUTTON_BG, width=BUTTON_WIDTH)
+
+        add_btn = Button(self, text="ADD TO LIST", command=self.add_songs_to_list, bg=BUTTON_BG, width=BUTTON_WIDTH)
         add_btn.grid(row=1, column=0)
+
+        play_btn = Button(self, text="PLAY SONG", command=self.play_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
+        play_btn.grid(row=2, column=0)
+
+        pause_btn = Button(self, text="PAUSE/UNPAUSE", command=self.pause_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
+        pause_btn.grid(row=3, column=0)
+
+        prev_btn = Button(self, text="PREVIOUS SONG", command=self.play_prev_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
+        prev_btn.grid(row=4, column=0)
+
+        next_btn = Button(self, text="NEXT SONG", command=self.play_next_song, bg=BUTTON_BG, width=BUTTON_WIDTH)
+        next_btn.grid(row=5, column=0)
 
         self.message_label = Label(self, fg='Black', font=('Helvetica 12 bold italic', 10), bg='ivory2')
         self.message_label.grid(row=6, column=0)
@@ -53,123 +75,110 @@ class FrameApp(Frame):
         self.songs_list = list()
         self.pausing = False
         self.list_index = 0
-        self.SONG_END = pygame.USEREVENT + 1
+        self.song_end = pygame.USEREVENT + 1  # TODO: should be lower case? Doesn't change through the runtime
 
+    #################################################################################
+    def add_songs_to_list(self):
+        try:
+            directory = askopenfilenames()
 
-#################################################################################
-def add_songs_to_list():
-    try:
-        app.text_list.delete(0.0, END)
-        directory = askopenfilenames()
-        for song_dir in directory:
-            print(song_dir)
-            song_metadata = {'title': get_song_name(song_dir), 'artist': get_random_artist()}
-            app.songs_list.append(Song(song_dir, song_metadata))
+            for song_dir in directory:
+                logging.info(song_dir)
+                self.songs_list.append(Song(song_dir, get_song_name(song_dir), get_random_artist()))
 
-        for key, item in enumerate(app.songs_list):
-            # song = EasyID3(item)
-            song = app.songs_list[key]
-            song_data = (str(key + 1) + ' : ' + song.song_metadata['title'] + ' - ' + song.song_metadata['artist'])
-            app.text_list.insert(END, song_data + '\n')
+            self.text_list.delete(0.0, END)
 
-    except:
-        print("an error occurred")
+            for index, song in enumerate(self.songs_list):
+                # song = EasyID3(item)
+                song_data = f"{index + 1}: {song.title} - {song.artist}"
+                self.text_list.insert(END, song_data + '\n')
 
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
 
-#################################################################################
-def get_song_data():
-    try:
-        song = app.songs_list[app.list_index]
-        song_data = f"Now playing: {song.song_metadata['title']} - {song.song_metadata['artist']} " \
-                    f"Nr:{str(app.list_index + 1)} "
-        return song_data
+    #################################################################################
+    def get_song_data(self):
+        try:
+            song = self.songs_list[self.list_index]
+            song_data = f"Now playing: Nr:{self.list_index + 1} {song.title} - {song.artist} "
 
-    except:
-        print("failed to get song data")
+            return song_data
 
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
 
-#################################################################################
-def play_song():
-    try:
-        directory = app.songs_list[app.list_index].song_path
-        pygame.mixer.music.load(directory)
-        pygame.mixer.music.play(1, 0.0)
-        pygame.mixer.music.set_endevent(app.SONG_END)
-        app.pausing = False
-        app.message_label['text'] = get_song_data()
+    #################################################################################
+    def play_song(self):
+        try:
+            directory = self.songs_list[app.list_index].path
+            play_music(directory, self.song_end)
+            self.pausing = False
+            self.message_label['text'] = self.get_song_data()
 
-    except:
-        print("play song btn")
-
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
 
 #################################################################################
-def check_music():
-    try:
-        for event in pygame.event.get():
-            if event.type == app.SONG_END:
-                play_next_song()
+    def check_music(self):
+        try:
 
-    except:
-        print("failed to check song")
+            for event in pygame.event.get():
+                if event.type == self.song_end:
+                    self.play_next_song()
 
-
-#################################################################################
-def pause_song():
-    try:
-        if app.pausing:
-            pygame.mixer.music.unpause()
-            app.pausing = False
-        else:
-            pygame.mixer.music.pause()
-            app.pausing = True
-
-    except:
-        print("pause/unpause btn")
-
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
 
 #################################################################################
-def get_next_song():
-    try:
-        return (app.list_index + 1) % len(app.songs_list)
-        """if app.list_index + 1 < len(app.songs_list):
-            return app.list_index + 1
-        else:
-            return 0"""
-    except:
-        print("failed next song")
+    def pause_song(self):
+        try:
+            if self.pausing:
+                pygame.mixer.music.unpause()
+                self.pausing = False
+            else:
+                pygame.mixer.music.pause()
+                self.pausing = True
 
-
-#################################################################################
-def play_next_song():
-    try:
-        app.list_index = get_next_song()
-        play_song()
-
-    except:
-        print("failed next song")
-
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
 
 #################################################################################
-def get_previous_song():
-    try:
-        return (app.list_index - 1) % len(app.songs_list)
+    def get_next_song(self):
+        try:
 
-        """if app.list_index - 1 >= 0:
-            return app.list_index - 1
-        else:
-            return len(app.songs_list) - 1"""
-    except:
-        print("failed to previous song")
+            return (self.list_index + 1) % len(self.songs_list)
 
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
 
-#################################################################################
-def play_prev_song():
-    try:
-        app.list_index = get_previous_song()
-        play_song()
+    #################################################################################
+    def play_next_song(self):
+        try:
+            self.list_index = self.get_next_song()
+            self.play_song()
 
-    except:
-        print("prev song btn")
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
+
+    #################################################################################
+    def get_previous_song(self):
+        try:
+
+            return (self.list_index - 1) % len(self.songs_list)
+
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
+
+    #################################################################################
+    def play_prev_song(self):
+        try:
+            self.list_index = self.get_previous_song()
+            self.play_song()
+
+        except Exception as e:
+            logging.info(f"an error occurred:{e}")
+
+# TODO: seems redundant all the exceptions. Is there more efficient way to write handle exceptions?
 
 
 #################################################################################
@@ -187,5 +196,5 @@ app = FrameApp(window)
 #################################################################################
 while True:
     # runs mainloop of program
-    check_music()
+    app.check_music()
     app.update()
