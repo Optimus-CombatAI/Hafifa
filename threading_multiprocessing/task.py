0e34feab3756adfdc3372a2d90fed13cfb8abf42
Maxim <wainermaxim@gmail.com>
2025-11-25 17:19:33 +0200

    add fixes

--- threading_multiprocessing/task.py
+++ threading_multiprocessing/task.py
@@ -1,69 +1,59 @@
-import yfinance as yf
-from dotenv import load_dotenv
-import os
 import pandas as pd
 from datetime import datetime, timedelta
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, Future
 
-load_dotenv()
-
-DESTINATION_FILE = os.getenv("DESTINATION_FILE", "destination.txt")
-SOURCE_FILE = os.getenv("SOURCE_FILE", "source.txt")
-
-BTC_TICKER = yf.Ticker("BTC-USD")
+from constants import BTC_TICKER, DESTINATION_FILE, SOURCE_FILE
 
 
-def get_dates(file_path: str) -> list[datetime]:
-    """
-    another possibility
-    unique_dates = {line.strip().split()[0] for line in file}
-    dates_list = [datetime(*map(int, date.split("-"))) for date in unique_dates]
-    """
-    with open(file_path) as source_file:
-        unique_dates = {line.strip().split()[0] for line in source_file}
-        dates_list = []
+def prepare_data_frame(futures: list[Future]) -> pd.DataFrame:
+    df = pd.concat([future.result() for future in futures])
 
-    for date in unique_dates:
-        year, month, day = date.split("-")
-        dates_list.append(datetime(int(year), int(month), int(day)))
+    return df.reset_index()
 
-    return dates_list
+
+def clean_up_df(data_frame: pd.DataFrame) -> pd.DataFrame:
+    data_frame['date'] = data_frame['raw_line'].str.split().str[0]
+    data_frame['date'] = pd.to_datetime(data_frame['date'], format="%Y-%m-%d")
+
+    return data_frame[['date']].drop_duplicates().sort_values(by='date').reset_index(drop=True)
+
+
+def get_dates(file_path: str) -> pd.DataFrame:
+    df = pd.read_csv(file_path, header=None, names=['raw_line'])
+    clean_df = clean_up_df(df)
+
+    return clean_df
 
 
 def get_next_date(date: datetime) -> datetime:
     return date + timedelta(days=1)
 
 
-def fetch_data(date, results) -> None:
-    data = BTC_TICKER.history(start=date, end=get_next_date(date))
-    results[date] = data
+def fetch_data(date: datetime) -> None:
+    return BTC_TICKER.history(start=date, end=get_next_date(date))
 
 
-def write_to_csv(results: dict) -> None:
-    sorted_results = dict(sorted(results.items()))
+def add_percentage(data_frame: pd.DataFrame) -> None:
+    data_frame["percent_change"] = data_frame["Close"].pct_change() * 100
+    data_frame["percent_change"] = data_frame["percent_change"].fillna(0)
 
-    combined_df = pd.concat(sorted_results.values())
-    combined_df = combined_df.reset_index()
 
-    combined_df["percent_change"] = combined_df["Close"].pct_change() * 100
-    combined_df["percent_change"] = combined_df["percent_change"].fillna(0)
+def write_to_csv(stock_data: pd.DataFrame) -> None:
+    add_percentage(stock_data)
 
-    combined_df["stock_type"] = "BTC"
-
-    combined_df[["Date", "stock_type", "percent_change"]].to_csv(DESTINATION_FILE)
+    stock_data["stock_type"] = "BTC"
+    stock_data[["Date", "stock_type", "percent_change"]].to_csv(DESTINATION_FILE)
 
 
 def main() -> None:
-
     dates = get_dates(SOURCE_FILE)
 
-    results = {}
-
     with ThreadPoolExecutor() as exe:
-        for date in dates:
-            exe.submit(fetch_data, date, results)
+        futures = [exe.submit(fetch_data, date) for date in dates['date']]
 
-    write_to_csv(results)
+    btc_data = prepare_data_frame(futures)
+    
+    write_to_csv(btc_data)
 
 
 if __name__ == '__main__':
