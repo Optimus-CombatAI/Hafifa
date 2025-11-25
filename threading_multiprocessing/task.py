3f0c03914b2e6d3715525322aad93645aba60b6b
Maxim <wainermaxim@gmail.com>
2025-11-25 17:43:56 +0200

    add more fixes

--- threading_multiprocessing/task.py
+++ threading_multiprocessing/task.py
@@ -5,22 +5,22 @@ from concurrent.futures import ThreadPoolExecutor, Future
 from constants import BTC_TICKER, DESTINATION_FILE, SOURCE_FILE
 
 
-def prepare_data_frame(futures: list[Future]) -> pd.DataFrame:
-    df = pd.concat([future.result() for future in futures])
+def prepare_stock_data_df(futures: list[Future]) -> pd.DataFrame:
+    combined = pd.concat([future.result() for future in futures])
 
-    return df.reset_index()
+    return combined.reset_index()
 
 
-def clean_up_df(data_frame: pd.DataFrame) -> pd.DataFrame:
-    data_frame['date'] = data_frame['raw_line'].str.split().str[0]
-    data_frame['date'] = pd.to_datetime(data_frame['date'], format="%Y-%m-%d")
+def clean_up_dates(dates_df: pd.DataFrame) -> pd.DataFrame:
+    dates_df['date'] = pd.to_datetime(dates_df['raw_line'], format="%Y-%m-%d %H:%M:%S.%f")
+    dates_df['date'] = dates_df['date'].dt.normalize()
 
-    return data_frame[['date']].drop_duplicates().sort_values(by='date').reset_index(drop=True)
+    return dates_df[['date']].drop_duplicates().sort_values(by='date').reset_index(drop=True)
 
 
 def get_dates(file_path: str) -> pd.DataFrame:
     df = pd.read_csv(file_path, header=None, names=['raw_line'])
-    clean_df = clean_up_df(df)
+    clean_df = clean_up_dates(df)
 
     return clean_df
 
@@ -33,9 +33,9 @@ def fetch_data(date: datetime) -> None:
     return BTC_TICKER.history(start=date, end=get_next_date(date))
 
 
-def add_percentage(data_frame: pd.DataFrame) -> None:
-    data_frame["percent_change"] = data_frame["Close"].pct_change() * 100
-    data_frame["percent_change"] = data_frame["percent_change"].fillna(0)
+def add_percentage(dates_df: pd.DataFrame) -> None:
+    dates_df["percent_change"] = dates_df["Close"].pct_change() * 100
+    dates_df["percent_change"] = dates_df["percent_change"].fillna(0)
 
 
 def write_to_csv(stock_data: pd.DataFrame) -> None:
@@ -51,8 +51,8 @@ def main() -> None:
     with ThreadPoolExecutor() as exe:
         futures = [exe.submit(fetch_data, date) for date in dates['date']]
 
-    btc_data = prepare_data_frame(futures)
-    
+    btc_data = prepare_stock_data_df(futures)
+
     write_to_csv(btc_data)
 
 
