7d049c8a3500cb215dd6e77d3e6b3a6a8fdc4c6f
Maxim <wainermaxim@gmail.com>
2025-12-09 12:07:48 +0200

    async main

--- rolling_exercise/main.py
+++ rolling_exercise/main.py
@@ -1,15 +1,24 @@
+import asyncio
 from fastapi import FastAPI
 
-from db.db_functions import set_up_db, fill_dummy_data
+from db.db_functions import set_up_db, fill_dummy_data, close_session
 from routers.air_quality_router import router as air_quality_router
 from routers.aqi_router import router as aqi_router
 from routers.alerts_router import router as alerts_router
 
 
-set_up_db()
-fill_dummy_data()
-
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def on_startup():
+    await set_up_db()
+    await fill_dummy_data()
+
+
+@app.on_event("shutdown")
+def on_shutdown():
+    close_session()
 
 
 app.include_router(air_quality_router)
